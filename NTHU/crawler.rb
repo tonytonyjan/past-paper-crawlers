901cd559c1251265a14a82ac6d8e309c22cd8d89
Tony Jian <tonytonyjan@gmail.com>
2013-06-05 16:54:10 +0800

    fix url bug

--- NTHU/crawler.rb
+++ NTHU/crawler.rb
@@ -8,15 +8,15 @@ url = "http://www.lib.nthu.edu.tw/library/department/ref/exam/"
 file_counter = 0
 Nokogiri::HTML(open(url)).css('div#wrapper > div#cwrp.clearfix > div > ul > li > ul > li > a').each{|a|
   department = a.text.strip
-  url = URI.join(url, a[:href])
-  Nokogiri::HTML(open(url)).css('div#wrapper>div#cwrp.clearfix>table.listview>tbody>tr')[1..-1].each{|tr|
+  url2 = URI.join(url, a[:href])
+  Nokogiri::HTML(open(url2)).css('div#wrapper>div#cwrp.clearfix>table.listview>tbody>tr')[1..-1].each{|tr|
     year = tr.css('td:nth-child(1)').text.to_i + 1911
     tr.css('td:nth-child(2)>ul>li').each{|li|
       links = li.css('a')
       program = li.child.text[/(.*)：/, 1]
       links.each{|a|
         subject = a.text.strip
-        file_url = URI.join(url, a[:href]).to_s
+        file_url = URI.join(url2, a[:href]).to_s
         open(file_url){|f|
           file_name = (f.meta['content-disposition'] && f.meta['content-disposition'][/filename="(.*)"/, 1]) || File.basename(file_url)
           file_path = File.join('files', "#{file_counter += 1}-#{file_name}")
